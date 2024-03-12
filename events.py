a9de694d16232f384820239ecfb3e3df258d866b
Kasmik004 <67088222+Kasmik004@users.noreply.github.com>
2024-03-12 18:59:35 +0545

    add socket
    added socket functionality and fixed errrors

--- events.py
+++ events.py
@@ -1,14 +1,11 @@
 from extensions import socketio
 from flask import Blueprint, request, render_template, redirect, url_for
+from models.Users import User, ChatBox, Chats
 from extensions import db
-from models.Users import User
-from models.video import Video
-from models.Users import ChatBox, Chats
 
 @socketio.on("connect")
 def handle_connect():
     print("Client Connected")
-
 
 @socketio.on("new_message")
 def handle_new_message(message, current_box, current_user):
@@ -18,9 +15,10 @@ def handle_new_message(message, current_box, current_user):
         chat = Chats(chat=message,u_id=user.id,chat_box_id=box.chat_box_id)
         db.session.add(chat)
         db.session.commit()
-        print("done boss")
+        print("done boss") 
         url = "/home/box/{user}/{box}".format(user=current_user, box=current_box)
         #return render_template(url_for('home.box',current_box=current_box,current_user=current_user))
 
-        return redirect(url)
+        socketio.emit('add_new_message', data={"chat": chat.chat, "user": current_user})
+    
 
