4bbbe80b05d6eb6b946a19c6ce12d6588b4ea260
Kasmik004 <rkasmik@gmail.com>
2024-03-19 22:57:15 +0545

    added print statement

--- __init__.py
+++ __init__.py
@@ -5,11 +5,11 @@ from routes.main import main
 from routes.api import home
 
 from events import socketio
-
+#new comment
 app =  Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SECRET_KEY'] = 'hard to guess string'
-
+print("configured with database")
 db.init_app(app)
 app.register_blueprint(main, url_prefix="/main")
 app.register_blueprint(home, url_prefix="/home")
